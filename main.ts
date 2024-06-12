4054e327f84bd50b70c8fc6bdd993694c7b8df20
Marwakhater <excellence.grad@gmail.com>
2024-06-12 12:54:11 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+cuteBot.moveTime(cuteBot.Direction.forward, 50, 5)
+basic.forever(function () {
+	
+})
